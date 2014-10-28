131726b7d3130b11e31efcbd9b07885c9595f560
Grant Crofton <grantac@hotmail.com>
2014-10-28 22:42:44 +0000

    Added initial files

---
+++ Turtles.fsx
@@ -0,0 +1,17 @@
+﻿#r @"packages\FParsec.1.0.1\lib\net40-client\FParsecCS.dll"
+#r @"packages\FParsec.1.0.1\lib\net40-client\FParsec.dll"
+
+#r "System.Drawing.dll"
+#r "System.Windows.Forms.dll"
+
+open FParsec
+
+
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+test pfloat "__"
+test pfloat "1.23"
