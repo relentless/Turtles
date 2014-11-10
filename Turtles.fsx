91b1a56d5468ab3807235621d82ec77a9841ec93
Grant Crofton <grantac@hotmail.com>
2014-11-10 14:57:59 +0000

    Added interpreter code

--- Turtles.fsx
+++ Turtles.fsx
@@ -25,16 +25,35 @@ let pblock = pstring "[" >>. many1 (pcommand .>> spaces) .>> pstring "]"
 do prepeatImpl := pstring "repeat" >>. spaces1 >>. pfloat .>> spaces .>>. pblock
                     |>> (fun (x,commands) -> Repeat(int x, commands))
 
-// test the parsers
-let test p str =
-    match run p str with
-    | Success(result, _, _)   -> printfn "Success: %A" result
-    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+let parse code =
+    match run pcommand code with
+    | Success(result, _, _) -> [result]
+    | Failure(errorMsg, _, _) -> failwith ("Parse error: " + errorMsg)
 
-test pforward "forward 6"
-test pleft "left 6"
-test pright "right 2"
-test pblock "[left 3 forward 2]"
-test pcommand "repeat 3 [left 30 repeat 5 [forward 50 right 30]]"
+parse "repeat 3 [left 30 repeat 5 [forward 50 right 30]]"
 
 // INTERPRETER
+type Turtle = { X: int; Y: int; Direction: int }
+let startTurtle = { X=500; Y=500; Direction=0 }
+
+let execute code =
+    
+    let rec exec codeToExec currentTurtle points =
+        //printfn "Code: %A\nTurtle: %A" codeToExec currentTurtle
+        match codeToExec with
+        | [] -> currentTurtle,points
+        | currentInstruction::rest ->
+                match currentInstruction with
+                | Turn(angle) -> 
+                    exec rest { currentTurtle with Direction = (currentTurtle.Direction + angle) } points
+                | Forward(distance) -> 
+                    exec rest { currentTurtle with Y = currentTurtle.Y + distance } ((currentTurtle.X, currentTurtle.Y + distance)::points)
+                | Repeat(count, commands) -> 
+                    let flattenedCommands = commands |> List.replicate count |> List.concat
+                    exec (flattenedCommands@rest) currentTurtle points
+
+    exec code startTurtle [(startTurtle.X, startTurtle.Y)]
+
+"repeat 3 [forward 5 repeat 4 [forward 1]]"
+|> parse
+|> execute
